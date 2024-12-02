cde138c6332582b7ae4acbc40c8e7d85b6c28ca1
User <user@MacBook-Pro-de-DFranquias.local>
2024-12-02 20:17:29 -0300

    adjust indexdts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module 'react-react-native-not-rapid-click' {
-    export function useDebounceFunction(fn: Function, delay: number): (...args: any[]) => void;
+    export function useNotRapidClick(fn: Function, delay: number): (...args: any[]) => void;
   }
   
